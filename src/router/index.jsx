6438bcd545058139b15cd3f952c2de5a3edc538e
a072682 <a072682@gmail.com>
2026-04-26 04:05:53 +0800

    chore: 暫時關閉非儀表板頁面，專注穩定儀表板功能

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,10 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import FrontLayout from "../layouts/FrontLayout";
 import PrivateRoute from "../components/common/PrivateRoute";
 import DashboardPage from "../pages/DashboardPage";
-import AnalysisPage from "../pages/AnalysisPage";
-import HistoryPage from "../pages/HistoryPage";
+// import AnalysisPage from "../pages/AnalysisPage";   // TODO: 待儀表板穩定後開放
+// import HistoryPage from "../pages/HistoryPage";     // TODO: 待儀表板穩定後開放
 import SettingsPage from "../pages/SettingsPage";
-import SimulationPage from "../pages/SimulationPage";
+// import SimulationPage from "../pages/SimulationPage"; // TODO: 待儀表板穩定後開放
 import LoginPage from "../pages/LoginPage";
 import NotFound from "../pages/NotFound";
 
@@ -19,10 +19,14 @@ const router = createBrowserRouter([
         element: <FrontLayout />,
         children: [
           { index: true, element: <DashboardPage /> },
-          { path: "analysis", element: <AnalysisPage /> },
-          { path: "history", element: <HistoryPage /> },
+          // TODO: 待儀表板穩定後開放
+          // { path: "analysis", element: <AnalysisPage /> },
+          // TODO: 待儀表板穩定後開放
+          // { path: "history", element: <HistoryPage /> },
+          // 保留 settings：儀表板測試期間需使用「立即計算今日評分」按鈕
           { path: "settings", element: <SettingsPage /> },
-          { path: "simulation", element: <SimulationPage /> },
+          // TODO: 待儀表板穩定後開放
+          // { path: "simulation", element: <SimulationPage /> },
           { path: "*", element: <NotFound /> },
         ],
       },
